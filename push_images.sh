a6e9c287f4d0e0fdb4e843c2347069f04b46e16a
William Konitzer <william@konitzer.co.uk>
2023-11-13 02:36:49 -0600

    add logging

--- push_images.sh
+++ push_images.sh
@@ -3,16 +3,24 @@
 # Variables
 REGISTRY="mirantis.azurecr.io"
 PARALLEL_UPLOADS=5  # Number of parallel uploads
+LOG_FILE="/tmp/push_images.log"
 
 # Function to push the image
 push_image() {
   local image=$1
   echo "Pushing $image"
-  docker push "$image"
+  if docker push "$image" 2>>"$LOG_FILE"; then
+    echo "Successfully pushed $image" >>"$LOG_FILE"
+  else
+    echo "Failed to push $image" >>"$LOG_FILE"
+  fi
 }
 
 # Initialize counter
 counter=0
+
+# Clear the log file at the start of the script
+> "$LOG_FILE"
 
 # List all images from the mirantis.azurecr.io registry and push them to the local registry
 docker image ls --format "{{.Repository}}:{{.Tag}}" | grep "^$REGISTRY" | while read -r image; do
@@ -32,4 +40,3 @@ done
 wait
 
 echo "All images have been pushed to the local registry."
-
